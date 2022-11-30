327b169dec9a0d3f2719de12805f5173be48e7b5
iLinks <3043564076@qq.com>
2022-11-30 17:53:26 +0800

    随便改了点

--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -3,13 +3,27 @@
 
 <!-- <el-backtop target=".page-component__scroll .el-scrollbar__wrap"></el-backtop> -->
 <div class="line"></div>
+
+<!-- <div id="top_div"> -->
+<div>
+<div class="background">
+      <img src="../assets/1.jpeg" width="40%" height="80%" alt=""/>
+</div>
+<div class="front">
+  <br>
+  <p id="title"><i class="el-icon-s-management"></i> Intelli Sci</p>
+  <br>
+</div>
+</div>
+
+<br>
 <el-menu
   :default-active="activeIndex2"
   class="el-menu-demo"
   mode="horizontal"
   @select="handleSelect"
-  background-color="#3365de"
-  text-color="#fff"
+  background-color="#abadad"
+  text-color="#333333"
   active-text-color="#ffd04b">
 
   <!-- <el-menu-item index="1">处理中心</el-menu-item> -->
@@ -27,15 +41,27 @@
   </el-submenu> -->
 
 <el-menu-item index="5">主页</el-menu-item>
-  <el-menu-item index="3">登录/注册</el-menu-item>
+  <el-menu-item index="3" @click="goto_login()">登录/注册</el-menu-item>
   <el-menu-item index="4">个人中心</el-menu-item>
-
 </el-menu>
+
+
+
 
 <br>
 <el-input v-model="input" placeholder="中文文献、外文文献" size="big"> 
+  <el-select v-model="select" slot="prepend" placeholder="篇名">
+      <el-option label="篇名" value="1"></el-option>
+      <el-option label="主题" value="2"></el-option>
+      <el-option label="作者" value="3"></el-option>
+      <el-option label="全文" value="4"></el-option>
+      <el-option label="关键词" value="5"></el-option>
+    </el-select>
     <el-button slot="append" icon="el-icon-search" @click="show_input()"></el-button>
 </el-input>
+
+<br>
+
 <br><br>
 
   <!-- <el-container>
@@ -46,12 +72,25 @@
 <div id="divs">
   <div class="div" id="div1">
 推荐页1
+<br><br>
+标题1
+<br>
+内容内容内容内容<br>
+内容内容内容内容<br>
   </div>
   <div class="div" id="div2">
 推荐页2
+<br><br>
+标题2<br>
+内容内容内容内容<br>
+内容内容内容内容<br>
   </div>
   <div class="div" id="div3">
 推荐页3
+<br><br>
+标题3<br>
+内容内容内容内容<br>
+内容内容内容内容<br>
   </div>
 </div>
 
@@ -72,7 +111,8 @@
       return {
         activeIndex: '1',
         activeIndex2: '1',
-        input: ''
+        input: '',
+        select: ''
       };
     },
     methods: {
@@ -81,6 +121,9 @@
       },
       show_input() {
           console.log(this.input);
+      },
+      goto_login() {
+        this.$router.push({ path: '/login' });
       }
 
     }
@@ -88,6 +131,10 @@
 </script>
 
 <style scoped>
+.el-menu-demo {
+  /* height: 80px; */
+}
+
 .el-header, .el-footer {
     background-color: #B3C0D1;
     color: #333;
@@ -124,6 +171,9 @@
 
 .el-input {
     width: 800px;
+  }
+  .el-select{
+    width: 100px;
   }
 
 /* #div1 {
@@ -164,5 +214,31 @@
     font-size: 15px;
     color: #B3C0D1;
 }
+
+#title {
+  font-size: 50px;
+  font-weight: 900;
+  color: #3365de;
+}
+
+.background {
+  /* background-color: yellow; */
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  position: absolute;
+  width: 100%;
+  height: 35%;
+  opacity: 30%;
+}
+
+.front {
+  z-index: 1;
+  position: relative; /*调成absolute时没有居中而是靠左，所以我试了试改成relative了*/
+}
+
+/* .title_tuijianye {
+  font-size: 20px;
+} */
 
 </style>
